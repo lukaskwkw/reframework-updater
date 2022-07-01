85ae16baf2ecd8686f8115980a778566996098b2
luqlap <luqlap@example.com>
2022-07-01 21:22:30 +0200

    feat(REFR-GITHUB-API): Add github api support for reframework release

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![feature(option_result_contains)]
 
-use crate::{steam::get_games_locations};
-use crate::utils::{local_version::getLocalVersions, version_parser, fetch::fetch_file};
+use crate::refr_github::REFRGithub;
+use crate::steam::get_games_locations;
 use crate::toml::config::{deserialize, serialize};
+use crate::utils::{fetch::fetch_file, local_version::getLocalVersions, version_parser};
+#[cfg(target_os = "windows")]
+use mslnk::ShellLink;
 use scrapper::scrapper::scrape_latest_data;
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -12,13 +15,12 @@ use std::{
     time::Duration,
 };
 use unzip::unzip::unzip;
-#[cfg(target_os = "windows")]
-use mslnk::ShellLink;
 
+mod refr_github;
 mod utils {
+    pub mod fetch;
     pub mod local_version;
     pub mod version_parser;
-    pub mod fetch;
 }
 mod scrapper {
     pub mod ScrapperError;
@@ -33,9 +35,9 @@ pub mod unzip {
 mod tests {
     pub mod data;
 }
-mod toml { 
-    pub mod config;
+mod toml {
     pub mod FromValue;
+    pub mod config;
 }
 
 pub type VerResult = Result<String, Box<dyn Error>>;
@@ -52,11 +54,10 @@ pub fn create_ms_lnk(target: impl AsRef<Path>, arguments: Option<String>) {
     sl.set_arguments(arguments);
     // sl.set_arguments(Some("-private-window".to_owned()));
     sl.create_lnk(lnk).unwrap();
-
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn error::Error>> {
+// #[tokio::main]
+fn main() -> Result<(), Box<dyn error::Error>> {
     // let file_content = std::fs::read_to_string("./src/tests/releases_nightly.htm").unwrap();
     let args: Vec<_> = std::env::args().collect();
     for argument in std::env::args() {
@@ -71,11 +72,32 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         "DELETE_OPENVR_API_DLL_IF_YOU_WANT_TO_USE_OPENXR",
     ];
 
-    // let executable = PathBuf::from(args.get(0).unwrap());
-    // let executable = PathBuf::from(args.get(0).unwrap());
-    let exe_absolute_path = fs::canonicalize(&args[0])?;
+    println!("Before");
+    let ref_github = REFRGithub::new("REFramework-nightly", "praydog", "TDB");
 
-    println!("Absolute Path {:?}", exe_absolute_path);
+    let release = ref_github.get_reframework_latest_release()?;
+    println!("release TBD{:?}", release);
+    // download_release_asset(&release.assets.first().unwrap());
+    // println!("Before RTX");
+    // let release = get_reframework_latest_release("REFramework-nightly")?;
+    // println!("{:?}", release.assets);
+
+    // if isRepoVersionNewer(release.name, );
+    // let config = ReleaseListBuilder::
+    // let mut listBuilder = github::ReleaseList::configure();
+    // let list = listBuilder
+    //     .repo_name("REFramework-nightly")
+    //     .repo_owner("praydog")
+    //     .build()
+    //     .unwrap();
+
+    // let releases = list.fetch().unwrap();
+    // println!("{:?}", first);
+
+    // let list = t.build().unwrap();
+    // t.repo_name("REFramework-nightly").repo_owner("praydog");
+    // let executable = PathBuf::from(args.get(0).unwrap());
+    // let executable = PathBuf::from(args.get(0).unwrap());
 
     #[cfg(target_os = "windows")]
     create_ms_lnk(&args[0], Some("-run RE7".to_owned()));
@@ -125,4 +147,14 @@ re7.exe 418370
 re8.exe 1196590
 re3.exe 952060
 re2.exe 883710
+
+DMC5
+MHRISE
+RE2
+RE2_TDB66
+RE3
+RE3_TDB67
+RE7
+RE7_TDB49
+RE8
 */
