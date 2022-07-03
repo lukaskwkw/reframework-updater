f82e1bb3643ff66ea70a9be3518c7206a4bb6cd1
lukaskwkw <lukaskwkw@gmail.com>
2022-07-03 17:56:52 +0200

    refactor(STEAM): Code refactoring to support for Manager

--- src/steam.rs
+++ src/steam.rs
@@ -1,25 +1,32 @@
+use crate::DynResult;
+use game_scanner::{manager, prelude::Game, steam};
 use std::path::PathBuf;
 
-use game_scanner::{manager, prelude::Game, steam};
+pub struct SteamManager;
 
-use crate::DynResult;
-
-pub fn get_games_locations(game_ids: Vec<&str>) -> DynResult<Vec<PathBuf>> {
-    let games = steam::games()?;
-
-    let paths: Vec<PathBuf> = games
-        .iter()
-        .filter_map(
-            |game| match game_ids.iter().any(|id| id.to_owned() == game.id) {
-                true => Some(game.path.to_owned()?),
-                false => None,
-            },
-        )
-        .collect();
-    return Ok(paths);
+pub trait SteamThings {
+    fn get_games_locations(&self, game_ids: &Vec<&str>) -> DynResult<Vec<(String, PathBuf)>>;
+    fn run_game(&self, game: &Game) -> DynResult<()>;
 }
 
-pub fn run_game(game: &Game) -> DynResult<()> {
-    manager::launch_game(game)?;
-    Ok(())
+impl SteamThings for SteamManager {
+    fn get_games_locations(&self, game_ids: &Vec<&str>) -> DynResult<Vec<(String, PathBuf)>> {
+        let games = steam::games()?;
+
+        let game_path_vec: Vec<(String, PathBuf)> = games
+            .iter()
+            .filter_map(
+                |game| match game_ids.iter().any(|id| id.to_owned() == game.id) {
+                    true => Some((game.id.clone(), game.path.to_owned()?)),
+                    false => None,
+                },
+            )
+            .collect();
+        return Ok(game_path_vec);
+    }
+
+    fn run_game(&self, game: &Game) -> DynResult<()> {
+        manager::launch_game(game)?;
+        Ok(())
+    }
 }
