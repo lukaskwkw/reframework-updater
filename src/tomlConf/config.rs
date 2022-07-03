24af9f1727f9f38ebfe125f4969c86ee456b0f28
lukaskwkw <lukaskwkw@gmail.com>
2022-07-03 17:54:00 +0200

    feat(CONFIG): Add new fields to config structure, code refactoring

---
+++ src/tomlConf/config.rs
@@ -0,0 +1,55 @@
+use error_stack::{Context, IntoReport, Report, Result, ResultExt};
+use std::fs::File;
+use std::io::prelude::*;
+use std::{
+    fs::{self},
+    path::Path,
+};
+
+use super::configStruct::{
+    ConfigError::{self, *},
+    REvilConfig,
+};
+use super::utils::{deserialize, serialize};
+
+type ConfigResult<T> = Result<T, ConfigError>;
+
+pub struct REvilConfigProvider {
+    filename: String,
+}
+
+pub trait ConfigProvider {
+    fn load_from_file(&self) -> ConfigResult<REvilConfig>;
+    fn save_to_file(&self, config: &REvilConfig) -> ConfigResult<()>;
+}
+
+impl ConfigProvider for REvilConfigProvider {
+    fn load_from_file(&self) -> ConfigResult<REvilConfig> {
+        let content = fs::read_to_string(&self.filename)
+            .report()
+            .change_context(ConfigFileError)
+            .attach_printable_lazy(|| format!("Error reading {}", &self.filename))?;
+
+        let (main, games) = deserialize(&content).change_context(DeserializerError)?;
+        Ok(REvilConfig { main, games })
+    }
+
+    fn save_to_file(&self, config: &REvilConfig) -> ConfigResult<()> {
+        let content = serialize(config)?;
+        let mut file = File::create(&self.filename)
+            .report()
+            .change_context(ConfigFileError)?;
+        file.write(content.as_bytes())
+            .report()
+            .change_context(ConfigFileError)?;
+        Ok(())
+    }
+}
+
+impl REvilConfigProvider {
+    pub fn new(path: impl AsRef<Path>) -> Self {
+        REvilConfigProvider {
+            filename: path.as_ref().to_str().unwrap().to_owned(),
+        }
+    }
+}
---
+++ src/tomlConf/configStruct.rs
@@ -0,0 +1,64 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fmt::{Debug}, error::Error,
+};
+
+pub type ConfigResult<T> = Result<T, ConfigError>;
+
+#[derive(Debug)]
+pub enum ConfigError {
+    ConfigFileError,
+    DeserializerError,
+    SerializerError,
+}
+
+impl std::fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("ConfigError")
+    }
+}
+
+impl Error for ConfigError {}
+
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum Runtime {
+    OpenVR,
+    OpenXR,
+}
+
+impl Runtime {
+    pub fn as_local_dll(&self) -> String {
+        match self {
+            Runtime::OpenVR => "openvr_api.dll".to_owned(),
+            Runtime::OpenXR => "openxr_loader.dll".to_owned(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct GameConfig {
+    pub location: Option<String>,
+    pub steamId: Option<String>,
+    pub versions: Option<Vec<String>>,
+    pub nextgen: Option<bool>,
+    pub runtime: Option<Runtime>,
+    pub runArgs: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Main {
+    pub sources: Option<Vec<String>>,
+    pub autoupdate: Option<bool>,
+    pub steamExePath: Option<String>,
+    pub steamGamesIdToSearchFor: Option<Vec<String>>,
+}
+
+pub type ShortGameName = String;
+
+#[derive(Debug)]
+pub struct REvilConfig {
+    pub main: Main,
+    pub games: HashMap<ShortGameName, GameConfig>,
+}
